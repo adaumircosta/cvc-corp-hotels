87a991244fb17aca0bab8bc33ea8509f5e2d550c
Adaumir Costa <adaumircosta@gmail.com>
2019-11-22 20:59:38 -0400

    Class de iniciação do microserviço

---
+++ src/main/java/br/com/cvc/client/HotelsCvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelsCvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelsCvcApplication.class, args);
+	}
+
+}
